cca80c8e51a4a81fca6377137fcdc35357b1a655
Itay Ostraich <itayost1@gmail.com>
2025-07-25 02:21:42 +0300

    components

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import ContactForm from '@/components/common/ContactForm';
+
+export default function ContactPage() {
+  return (
+    <div className="section">
+      <div className="container">
+        <ContactForm />
+      </div>
+    </div>
+  );
+}
